e34f4ada545f2dd9a897e2d7d98d86a5ca75d4ab
Guy Sandler <74838853+TriBird-GitHub@users.noreply.github.com>
2022-08-21 11:30:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
-from GoogleNews import GoogleNews
-from datetime import datetime, timedelta
-from pytz import timezone
-import pandas as pd
+##########################################################
+#this is the code explained and without the testing parts#
+##########################################################
 
-#GET A PERIOD OF TIME
-est = timezone('EST')
-today = datetime.now(est)
-yesterday = today - timedelta(days=1)
+from pygooglenews import GoogleNews #imports the api
+from datetime import datetime, timedelta #datetime (unused for now)
+from pytz import timezone #timezones (unused for now)
+import random #random for giving you a random article
 
-print("Today's date:", today)
-print("Yesterday's  date:", yesterday)
+#Get Time of today
+PST = timezone('EST')
+today = datetime.now(PST)
 
-#SET THE DATE RANGE
-googlenews=GoogleNews(start='08/09/2022',end=today.strftime("%m/%d/%Y"))
-#SET THE LANGUAGE
-googlenews.set_lang('en')
-#SET THE TOPIC
-googlenews.search('Science')
-#Get links
-googlenews.get_links()
-a = googlenews.get_links()
-#get total
-googlenews.total_count()
-#GET THE NEWS
-result=googlenews.result()
-#CONVERT THE RESULT TO A PANDAS DATAFRAME
-df=pd.DataFrame(result)
+#search
+gn = GoogleNews() #shorter name
+news = gn.topic_headlines("science") #searches for articles with the topic of science
 
-# print(df)
-# print(len(a))
-print()
-# googlenews = GoogleNews(start='02/01/2020',end='02/28/2020')
+entries = news["entries"]
 
-# print(result)
+#counter and list veriables
+count = 0
+Results = []
+
+#makes the final part
+for entry in news["entries"]:
+        if count > 50:
+                # one less then the last
+                break
+        count = count + 1
+
+        # print(str(count) + ". " + "title: " + entry["title"] + "link: " + entry["link"])
+        Results.append(str(count) + ". " + "title: " + entry["title"] + "link: " + entry["link"])
+        
+
+#randomize
+num = random.randint(0, len(Results))
+
+#give you the Result
+print(Results[num])
